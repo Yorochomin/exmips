bc960afc02c2015d17aaebe312d331b9f03c24d6
Nobutaka Kito <62008130+nkito@users.noreply.github.com>
2025-03-20 12:05:19 +0900

    Add files via upload

--- u-boot/build_uboot.sh
+++ u-boot/build_uboot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # For excuting following lines,
-# some developper tools, such as bison, flex, libssl-dev, mips-linux-gnu-gcc, are necessary.
+# some developper tools, such as bison, flex, libssl-dev, gcc-mips-linux-gnu, are necessary.
 
 git clone --depth 1 -b devadd/ar9344_aterm https://github.com/musashino-build/u-boot.git
 cd u-boot
@@ -15,5 +15,7 @@ cp u-boot.bin u-boot.resized.bin
 truncate -s 262144 u-boot.resized.bin
 
 wget https://downloads.openwrt.org/releases/24.10.0/targets/ath79/tiny/openwrt-24.10.0-ath79-tiny-nec_wg600hp-initramfs-kernel.bin
+wget https://downloads.openwrt.org/releases/24.10.0/targets/ath79/tiny/openwrt-24.10.0-ath79-tiny-nec_wg600hp-squashfs-sysupgrade.bin
 
-cat u-boot.resized.bin openwrt-24.10.0-ath79-tiny-nec_wg600hp-initramfs-kernel.bin > ../firm_u-boot.bin
+cat u-boot.resized.bin openwrt-24.10.0-ath79-tiny-nec_wg600hp-initramfs-kernel.bin    > ../firm_u-boot_initramfs.bin
+cat u-boot.resized.bin openwrt-24.10.0-ath79-tiny-nec_wg600hp-squashfs-sysupgrade.bin > ../firm_u-boot_squashfs.bin
