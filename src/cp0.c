f1ca837b00cd22543ffa7ccee35ce5a3501b6fc4
Nobutaka Kito <nkito@sist.chukyo-u.ac.jp>
2025-03-22 11:50:36 +0900

    CP0 counter behavior is changed.

--- src/cp0.c
+++ src/cp0.c
@@ -38,20 +38,18 @@ void storeC0Counter(struct stMachineState *pM, uintmx val){
 uintmx loadC0CounterPrecise(struct stMachineState *pM){
     uint64_t counter;
 
-//    pM->reg.c0_count_currenttime = getTimeInUSec(pM);
     counter  = pM->reg.c0_count_currenttime - pM->reg.c0_count_basetime;
     counter *= ((uint64_t) (FREQ_CPU/(CPU_FREQ_COUNT_RESOLUTION*1000*1000)));
-    counter += (pM->emu.nExecInsts - pM->reg.c0_count_ninst_in_ctime) * 2;
+    counter += (pM->emu.nExecInsts - pM->reg.c0_count_ninst_in_ctime) / CPU_FREQ_COUNT_RESOLUTION;
     return counter;
 }
 
 uintmx loadC0Counter(struct stMachineState *pM){
     uint64_t counter;
 
-//    pM->reg.c0_count_currenttime = getTimeInUSec(pM);
     counter  = pM->reg.c0_count_currenttime - pM->reg.c0_count_basetime;
     counter *= ((uint64_t) (FREQ_CPU/(CPU_FREQ_COUNT_RESOLUTION*1000*1000)));
-    counter += (pM->emu.nExecInsts - pM->reg.c0_count_ninst_in_ctime) * 2;
+    counter += (pM->emu.nExecInsts - pM->reg.c0_count_ninst_in_ctime) / CPU_FREQ_COUNT_RESOLUTION;
     return counter;
 }
 
